2ed734b41fd528a5943f611126232a0c00815bd4
Magesh Kuppan <tkmagesh77@gmail.com>
2026-05-21 14:23:38 +0530

    control flow - @for

--- first-app/src/app/app.ts
+++ first-app/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Greeter  } from "./greeter/greeter";
+import { Products } from "./products/products";
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    Greeter
+    Greeter,
+    Products
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
